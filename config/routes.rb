4c34439311dd97ba9e008b4c6864dbf34cfe7a38
Harritone <yourrasik@gmail.com>
2021-02-23 13:05:58 +1000

    Add: devise install user and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'activity/mine'
   get 'activity/feed'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
