912687d617edd168add8e532430ddee558bc2890
Harritone <yourrasik@gmail.com>
2021-02-23 19:27:28 +1000

    Add: users controller actions and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,123 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show edit destroy update ]
+  before_action :set_users, only: %i[ index ]
+
   def index
   end
 
-  def show
-  end
 
   def new
-  end
-
-  def create
+    @user = User.new
+    set_choces
   end
 
   def edit
+    set_choces
+  end
+
+  def me 
+    @user = current_user
+  end
+
+  def password 
+    @user = current_user
+  end
+
+  def update_password
+    @user = current_user
+    respond_to do |format|
+      if @user.update(user_password_params)
+        bypass_sign_in(@user)
+        format.html {
+          redirect_to my_password_path,
+          notice: 'Password was successfully updated.'
+        }
+      else
+        format.html {render :password}
+      end
+    end
+  end
+
+  def update_me
+    @user = current_user
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { 
+          redirect_to my_settings_path, 
+          notice: 'You information was successfully updated.'
+        }
+      end
+    end
+  end
+
+  def create
+    @user = User.unscoped.new(user_params.except('role'))
+    @user.account = current_account
+    @user.password = 'password'
+
+    respond_to do |format|
+      begin
+        if @user.valid? && @user.invite!(current_user)
+          @user.add_role user_params[:role].to_sym, current_account
+          format.html {
+            redirect_to account_users_path,
+            notice: 'User was sucessfully invited.'
+          }
+        else
+          set_choces
+          format.html { render :new }
+        end
+      rescue ActiveRecord::RecordNotUnique
+        flash[:alert] = 'Email should be unique'
+      end
+    end
   end
 
   def update
+    respond_to do |format|
+      if @user.update(user_params.except('role'))
+        format.html {
+          redirect_to account_users_path,
+          notice: 'User was successfully updated.'
+          }
+      else
+        set_choces
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { 
+        redirect_to account_users_path,
+        notice: 'User was successfully removed.'
+       }
+       format.json { head :no_content }
+    end
+  end
+  
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def set_users
+    @users = current_account.users
+  end
+
+  def set_choces
+    @choices = [['Admin', 'admin'], ['User', 'user']]
+  end
+
+  def user_password_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :role, :time_zone)
   end
 end
