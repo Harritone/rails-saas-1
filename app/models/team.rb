3cf13ca673fed59bf884ec601e6fa1691839d3dc
Harritone <yourrasik@gmail.com>
2021-02-26 11:19:01 +1000

    Add: days

--- app/models/team.rb
+++ app/models/team.rb
@@ -5,6 +5,7 @@ class Team < ApplicationRecord
   validates :name, presence: true
 
   has_many :team_memberships
+  has_many :days, class_name: 'DaysOfTheWeekMembership', dependent: :delete_all
   has_many :users, through: :team_memberships
   accepts_nested_attributes_for :days
 end
