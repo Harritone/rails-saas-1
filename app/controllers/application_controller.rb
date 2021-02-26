d473a854a7c2a8eab9b050a31edcf7e59dad8e47
Harritone <yourrasik@gmail.com>
2021-02-26 11:54:48 +1000

    Add: helper to get visible  teams

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,16 @@ class ApplicationController < ActionController::Base
     @current_date ||= session[:current_date]
   end
 
+  def visible_teams
+    @visible_teams ||=
+      if current_user.has_role? :admin, current_account
+        current_account.teams.includes(:users)
+      else
+        current_user.teams.includes(:users)
+      end
+    @visible_teams
+  end
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :error => exception.message
   end
